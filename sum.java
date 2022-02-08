77acfa2c6f8d1652cb88d461523535f8c662d0cf
Smit Vachhani <smitvachhani1571999@gmail.com>
2022-02-08 15:22:29 +0530

    add mondify  smit in sum file

--- sum.java
+++ sum.java
@@ -3,10 +3,6 @@ class sum
     public static void main(){
         int a = 10;
         int b = 20;
-<<<<<<< HEAD
-        System.out.println("sum : ",a+b);
-=======
         System.out.println("smit :",a+b);
->>>>>>> smit/multiply
     }
 }
